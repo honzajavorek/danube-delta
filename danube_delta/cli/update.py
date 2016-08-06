41ec13709da2cbda16c368142258eb8b04430fb6
Honza Javorek <mail@honzajavorek.cz>
2016-08-06 20:02:21 +0200

    Do not upgrade dependencies - this makes hard to go through 'blog update' on Windows

--- danube_delta/cli/update.py
+++ danube_delta/cli/update.py
@@ -8,4 +8,4 @@ def update():
     """Gets other people's changes from GitHub"""
 
     run('git pull --rebase origin master')
-    run('pip install -r requirements.txt --upgrade')
+    run('pip install -r requirements.txt')
